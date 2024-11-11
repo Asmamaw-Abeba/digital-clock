f108531992ac525cb45d8d221714350bb484fcc0
Asmamaw-Abeba <asmeabeba12@gmail.com>
2024-11-11 10:07:33 -0800

    change inner buttons to input element inorder to good for editing values

--- clock.js
+++ clock.js
@@ -3,10 +3,10 @@ function countSecond() {
   const s = document.querySelector('.second');
   const m = document.querySelector('.minute');
   const h = document.querySelector('.hour');
-  
-  let hour = 0;
-  let minute = 0;
-  let second = 0;
+
+  let hour = Number(h.value);
+  let minute = Number(m.value);
+  let second =  Number(s.value);
   
   setInterval(() => {
     second += 1;
@@ -20,15 +20,16 @@ function countSecond() {
         hour += 1;
       }
 
-    if (hour === 12) {
+    if (hour === 12 && minute === 60) {
       hour = 1;
     }  
 
-  s.innerText = second + 's';
-  m.innerText = minute + 'm';
-  h.innerText = hour + 'h';
+  s.value = second + 's';
+  m.value = minute + 'm';
+  h.value = hour + 'h';
 
-  }, 1000);  
+  }, 1000);
+  
 
   
 }
